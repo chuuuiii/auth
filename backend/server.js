7d500aad3946476d8921a5035d6e829d3ef69dda
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-10-31 09:59:44 +0800

    Server setup and db configuration

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+import cors from 'cors'
 
+dotenv.config();
 
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+//routes
+app.get('/api/user')
 
 app.get('/', (req, res) => {
   res.send("Server is ready")
 })
 
-app.listen(3000, () => {
-  console.log("Server started at http://localhost:3000")
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started at http://localhost:${PORT}`)
 })
