f8d81cd7dc965e40ec673faa9aaebcec2a5543c4
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-11-01 14:39:24 +0800

    user model added

---
+++ backend/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+// import bcrypt from 'bcrypt';
+// import validator from 'validator';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      minlength: [2, 'Name must be at least 2 characters long'],
+      maxlength: [50, 'Name must be at most 50 characters long'] 
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid email`
+      }
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be at least 6 characters long'],
+      select: false 
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'], 
+      default: 'user'
+    }
+  },
+  { timestamps: true }
+);
+
+
+
+const User = mongoose.model('User', userSchema);
+export default User;
