eb07dc78af92bb66c7ff4231c65fd7402853aa1d
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-11-02 10:23:45 +0800

    error fixed in signup controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,18 +1,37 @@
-// import User from "../models/user.model";
+import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
 
+//Generate JWT token
+const generateToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '1d' });
+};
 
-//all user
-const getUsers = (req, res) => {
-  res.json({ message: 'All user' })
-}
+// Get all users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+  // res.json({ message: 'All users' });
+};
 
-//login
+// Login user
 const loginUser = (req, res) => {
-  res.json({ message: 'Login user'})
-}
+  res.json({ message: 'Login user' });
+};
 
-const signupUser = (req, res) => {
-  res.json({ message: 'Signup user'})
-}
+// Signup user
+const signupUser = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const user = await User.signup(name, email, password, role);
+    const token = generateToken(user._id);
+    res.status(201).json({ success: true, data: { email: user.email, token }});
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
 export { getUsers, loginUser, signupUser };
