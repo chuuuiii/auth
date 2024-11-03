6618bce7b575105bbce4a7d40ab85eeebda6de04
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-11-03 13:38:18 +0800

    Font and hover added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,17 +3,21 @@ import { NavLink, Outlet } from 'react-router-dom';
 const Navbar = () => {
   return (
     <>
-      <div className='bg-gray-700 text-white shadow-md'>
-        <header className='container mx-auto px-4 py-3'>
+      <div className='text-gray-900 font-roboto text-2xl border-b shadow-md'>
+        <header className='container mx-auto px-9 py-6'>
           <nav className='flex items-center justify-between'>
             <div className='flex items-center space-x-4'>
-              <NavLink to='/' className='text-2xl font-semibold hover:text-gray-300'>
+              <NavLink to='/' className='text-2xl hover:text-gray-500'>
                 RD
               </NavLink>
-              <div>
-                <NavLink to='/login' className='px-2 hover:text-gray-300'>Log in</NavLink>
-                <NavLink to='/signup' className='px-2 hover:text-gray-300'>Sign up</NavLink>
-              </div>
+            </div>
+            <div className='flex space-x-4 ml-auto'>
+              <NavLink to='/login' className='px-2 hover:text-gray-500'>
+                Log in
+              </NavLink>
+              <NavLink to='/signup' className='px-2 hover:text-gray-500'>
+                Sign up
+              </NavLink>
             </div>
           </nav>
         </header>
