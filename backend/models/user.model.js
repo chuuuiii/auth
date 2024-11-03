c56e0f776a1b52c4ee301b9951b577242c3164c3
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-11-03 16:06:51 +0800

    user model updated

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -27,8 +27,8 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['user', 'admin'],
-      default: 'user',
+      enum: ['User', 'Admin'],
+      default: 'User',
     },
   },
   { timestamps: true }
