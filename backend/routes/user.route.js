8bb2c17b299a99f1b8b5ab84c96de8035b7130e1
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-11-05 08:23:36 +0800

    Protected route added

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { getUsers, loginUser, signupUser } from '../controllers/user.controller.js';
+import authMiddleware from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
 
-router.get('/users', getUsers);        
+router.get('/users', authMiddleware, getUsers);        
 router.post('/login', loginUser);     
 router.post('/signup', signupUser);    
 
