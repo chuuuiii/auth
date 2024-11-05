53fba694ffe0bd4eec9a3402310d435c56e8ab7f
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-11-05 13:35:51 +0800

    Toastify added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import AppRoute from "./routes/AppRoute"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 function App() {
  
@@ -6,6 +10,7 @@ function App() {
   return (
     <>
       <AppRoute />
+      <ToastContainer position='top-right' autoClose={3000} />
     </>
   )
 }
